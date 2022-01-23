727fa44c6d1d6ca99469f2fc76831b0d571421d7
anupART <94508205+anupART@users.noreply.github.com>
2022-01-23 13:41:08 +0530

    Add files via upload
    this is live ass.1.1

---
+++ ass_1.1.py
@@ -0,0 +1,88 @@
+
+# COMPARISON OPERATORS
+
+# a=27
+# b=8
+# a=25.67
+# b=67.27
+# a=112223
+# b=7228828
+# a=0.5677
+# b=0.526280
+
+
+# print(a==b)
+# print(a>=b)
+# print(a<=b)
+# print(a!=b)
+# print(a>b)
+# print(a<b)
+
+
+
+
+# ASSIGNMENT OPERATORS
+
+
+
+
+
+c=14
+d=65
+
+c+=77.56
+print(c)
+c-=29.8
+print(c)
+c*=7
+print(c)
+c**=3
+print(c)
+c/=122.77
+print(c)
+c//=22
+print(c)
+c%=12
+print(c)
+
+
+# print(c+d)
+# print(c-d)
+# print(c*d)
+# print(c/d)
+# print(c//d)
+# print(c%d)
+# print(c**2)
+
+
+# IF ELSE 
+
+# a.i if it snows i willl go out else i will not
+# b.if it rains frogs i will dance
+ 
+# snow=True
+
+# if snow:
+#     print("we will go out")
+# else:
+#     print("i will not go out")
+
+# rain=True
+
+# if rain:
+#     print("frogs will dance")
+# else:
+#     print("it will not dance ")
+
+
+# Mixed two sentence
+
+snow=True
+rain=True
+if snow:
+    print("we will go out")
+
+if rain:
+    print("frogs will dance")
+else:
+    print("i will not go out and not dance ")
