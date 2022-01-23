d52357774e7c191509979db05052cc6c340eb107
anupART <94508205+anupART@users.noreply.github.com>
2022-01-23 13:42:09 +0530

    Add files via upload
    this is assignment 2.2

---
+++ ass_2.2.py
@@ -0,0 +1,52 @@
+
+# INCREMENT BY 2 INPLACE OF 1
+
+# a=[22,32,12,11,"python"]
+# for a in range(0,5):
+#   print(a)
+  
+i = 0
+while i < 20:
+  print(i)
+  i += 2
+
+
+
+
+
+# TO MAKE FOR LOOP INFINITE
+
+
+# li= [1]
+# for x in li:
+#     li.append(x + 1)
+#     print(x)
+
+# i=0
+# while i< 5:
+#    print("i will runnn always")
+
+
+
+
+
+
+
+
+# HOW  TO ADD  VALUES IN TUPLES
+
+tu=(23,28,90,4571,89,"anup","mycap")
+convert=list(tu)
+convert.append("python56")
+tu=tuple(convert)
+print(tu)
+
+
+
+
+# tu3=(788,672,6272,"leraning")
+# add=("program",)
+# tu3+=add
+# print(tu3)
+
+
